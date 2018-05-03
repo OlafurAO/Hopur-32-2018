9d6843edaddac8130bc14216bcaa4901aadfb7e8
OlafurAO <olafurao16@ru.is>
2018-05-03 18:33:39 +0000

    Add files via upload

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BookStore.Models;
+using BookStore.Services;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AuthorService _authorService;
+
+        public HomeController()
+        {
+            _authorService = new AuthorService();
+        }
+
+        public IActionResult Index()
+        {
+            var authors = _authorService.GetAllAuthors();
+
+            return View(authors);
+        }
+
+        
+    }
+}
---
+++ BookStore/Models/ViewModels/AuthorListViewModel.cs
@@ -0,0 +1,10 @@
+namespace BookStore.Models.ViewModels
+{
+    public class AuthorListViewModel
+    {
+        public int ID { get; set;}
+
+        public string Name { get; set;}
+    }
+
+}
