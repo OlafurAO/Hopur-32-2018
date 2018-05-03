4ba17ecf2b5f0ab37bb4093780fd0b68b363f2c8
OlafurAO <olafurao16@ru.is>
2018-05-03 17:12:47 +0000

    Initial Commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using BookStore.Data.EntityModels;
+
+namespace BookStore.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(
+                    "Server=tcp:verklegt2.database.windows.net,1433;Initial Catalog=VLN2_2018_H32;Persist Security Info=False;User ID=VLN2_2018_H32_usr;Password=bigRe)48;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");                    
+        }
+    }
+}
