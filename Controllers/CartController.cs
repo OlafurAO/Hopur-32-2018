7b4b5dae0cc85181f78aee06e7523e053d5484dd
OlafurAO <olafurao16@ru.is>
2018-05-06 16:00:11 +0000

    test

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -5,15 +5,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BookStore.Data.EntityModels;
+using BookStore.Services;
 
 namespace BookStore.Controllers
 {
     public class CartController : Controller
     {
-         public IActionResult AddToCart(Book book)       
+        private BookService _bookService;
+
+        public CartController()
         {
-            Console.WriteLine("Hello World!");
-            return View();
+            _bookService = new BookService();
+        }
+         public IActionResult AddToCart(int? ID)       
+        {
+            var book = _bookService.GetAllBooks().Find(x => x.ID == ID);
+            Console.WriteLine(book);
+
+            return View(book);
         }
     }
 }
